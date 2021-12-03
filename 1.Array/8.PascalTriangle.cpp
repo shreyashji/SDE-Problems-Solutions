4ac7092eabd8cbcc48781ca0d06d7d9c9f0695bc
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2021-12-03 22:13:19 +0530

    3 dec code 9th solution program

---
+++ 1.Array/8.PascalTriangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> r(numRows);// create n dynamic vector which will be empty after that
+    // we know we have to genrate n rows
+    for(int i=0; i<numRows; i++){
+    r[i].resize(i+1);// no colns will always equal to the no. of rows,resizing empty vector to i+1
+    r[i][0]= r[i][i]=1; //first and last element will be 1 so we just initialised it
+            
+    //linearly traverse from first coln to the 2nd last colnj<i so 2 & 1 getting added
+    for(int j=1; j<i; j++)
+    r[i][j]=r[i-1][j-1]+r[i-1][j];
+        
+    }
+    //after entire addition return pascal triangle 2d vector
+    return r;    
+    }
+};
---
+++ 1.Array/9.NextPermutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size(),k,l; //find the size of the array initially,then wee need to find the breakpoint of so we can say that the first breakpoint will be at the 2nd last index,breakpoint is k
+        
+        for(k=n-2; k>=0 ; k--){ //linearly iterate from the 2nd last index
+            if(nums[k]< nums[k+1]){
+                break; //if we found breakpoint we just breakout
+            }
+        }
+        if(k<0){ //if there is no breakpoint we willl simply reverse it and done
+            reverse(nums.begin(),nums.end());
+        }
+//but if there is a breakpoint,we again linearly traverse from the back and find some one greater than 
+        else{
+            for(l=n-1; l>k; l--){
+                if(nums[l] > nums[k]){
+                    break; //once we find we will breakout
+                }
+            }
+        swap(nums[k],nums[l]); //so we swap it ,third step
+        //use the two pointer approach to reverse
+        reverse(nums.begin() + k + 1,nums.end()); //everything on the right will be reversed
+        }
+    }
+};
