9f1ed627893a1c664935bbd341cdb6890d1f76c1
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2021-12-26 20:07:50 +0530

    find starting point of the cycle in ll

---
+++ 4.LinkedList/34.FIND THE STARTING POINT OF THE CYCLE.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) //edge cases, ll is null,only one element
+            return NULL;
+        
+        ListNode *slow  = head;
+        ListNode *fast  = head;
+        ListNode *entry = head;
+        
+        while(fast->next && fast->next->next) { //define collison points,move till fast reaches null
+            slow = slow->next; //move slow by 1
+            fast = fast->next->next; //move fast by 2
+        if(slow==fast) {    // there is a cycle,collison point
+            while(slow !=entry) {  // found the entry location
+                //move slow & fast by one unit each,till they are not equal
+                slow = slow->next;
+                entry = entry->next;
+                }
+            return entry; //return collison points
+            }
+        }
+        return NULL; //if fast reaches null,means there was never ever a cycle
+    }
+};
