722427b699921a107c565e9dfb73993b1aaab92d
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2022-03-27 20:06:10 +0530

    adding graph problem

---
+++ 16.2 Graph Leetcode Problems/152. Find If Path Exists in Graph.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+public:
+//time=
+//space=
+    //indegree -outdegree = n-1
+    //indegree should be n-1 & outdegree 0,only inserting ,no outgoing
+   // [a,b]  1->2<-3 total n=3 ,trust count n-1=2 correct 1 slould not trust nobody
+    int findJudge(int n, vector<vector<int>>& trust) {//[1 to 3],[1 to 4]
+        //only needed indegree 7indegree should be n-1
+        vector<int>data(n+1, 0); //for indegree 
+        for(auto x: trust)
+        {
+            data[x[0]]--; //outdegree --
+            data[x[1]]++; //indegree ++
+        }
+        //now vector contains and indgree which is n-1 return index
+        for(int i=1; i<=n; i++){ //traverse vcetor 
+            int x = data[i]; //return index
+            if(x == n-1)
+                return i;
+        }
+        
+        return -1;
+    }
+};
