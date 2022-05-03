89b702c949104aced6fc49a8457c547cf60ffed8
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2022-05-03 11:12:11 +0530

    adding dp problem on LIS Number of longest increasing subsequence

---
+++ 17.Dynamic Programming/5.DP on Longest Increasing Subsequence/47. Number of Longest Increasing Subsequences.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+int findNumberOfLIS(vector<int> &arr)
+{
+	int n = arr.size();
+    vector<int> dp(n,1),cnt(n,1);
+	int maxi = 1;
+	for(int i=0; i<n ; i++){//O(N^2)
+		for(int prev = 0; prev< i ; prev++){
+			if(arr[prev]  <  arr[i] && 1 + dp[prev] > dp[i] ) {
+				dp[i] =  1 + dp[prev];
+				//inherit 
+				cnt[i] = cnt[prev];
+			}
+			else if(arr[prev] < arr[i] && 1+dp[prev] == dp[i]){
+				//smae length inccre the count
+				cnt[i] += cnt[prev]; 
+			}
+		}
+		maxi = max(maxi , dp[i]);
+	}
+	int nos=0;
+	for(int i=0; i<n ; i++){
+		if(dp[i] == maxi) nos += cnt[i]; 
+	}
+	return nos;
+}
